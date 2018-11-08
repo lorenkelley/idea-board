b68adf1a7a9588374b44aafdb31a1d4c7f58cf72
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-11-08 11:30:10 -0500

    created db and models

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose')
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/my-idea-board')
+
+mongoose.connection.once('open', () => {
+    console.log(`Mongoose has connected to MongoDB`)
+})
+mongoose.connection.on('error', (error) => {
+    console.error(`MongoDB connection error!!! ${error}`)
+    process.exit(-1)
+})
+
+module.exports = mongoose
